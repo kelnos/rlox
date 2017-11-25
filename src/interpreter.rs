ad7cfd9f947b247011e1aa1ec55731f8c95261b6
Brian J. Tarricone <brian@tarricone.org>
2017-11-25 00:10:28 -0800

    implement basic interpreter that handles arithmetic & comparisons

---
+++ src/interpreter.rs
@@ -0,0 +1,151 @@
+use std::error::Error;
+use std::fmt;
+
+use expression::*;
+use token::{TokenType, Token};
+use value::Value;
+
+struct State {
+}
+
+#[derive(Debug)]
+struct RuntimeError {
+    location: Token,
+    description: String,
+}
+
+impl RuntimeError {
+    pub fn new(location: Token, description: String) -> Box<RuntimeError> {
+        Box::new(RuntimeError {
+            location,
+            description,
+        })
+    }
+}
+
+impl fmt::Display for RuntimeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.description())
+    }
+}
+
+impl Error for RuntimeError {
+    fn description(&self) -> &str {
+        &self.description
+    }
+}
+
+pub fn interpret(expr: Expr) -> Result<Value, Box<Error>> {
+    let mut state = State {};
+    evaluate_expression(&mut state, expr)
+}
+
+fn evaluate_expression(state: &mut State, expr: Expr) -> Result<Value, Box<Error>> {
+    match expr {
+        Expr::Binary { left, operator, right } => evaluate_binary(state, *left, operator, *right),
+        Expr::Grouping { expression } => evaluate_grouping(state, *expression),
+        Expr::Literal { value } => evaluate_literal(state, value),
+        Expr::Unary { operator, right } => evaluate_unary(state, operator, *right),
+    }
+}
+
+fn evaluate_binary(state: &mut State, left: Expr, operator: Token, right: Expr) -> Result<Value, Box<Error>> {
+    let left_value = evaluate_expression(state, left)?;
+    let right_value = evaluate_expression(state, right)?;
+    match operator.token_type {
+        TokenType::Minus | TokenType::Plus | TokenType::Slash | TokenType::Star => arithmetic(&left_value, operator, &right_value),
+        TokenType::Less | TokenType::LessEqual | TokenType::Greater | TokenType::GreaterEqual => compare(&left_value, operator, &right_value),
+        TokenType::EqualEqual => Ok(Value::Boolean(is_equal(&left_value, &right_value))),
+        TokenType::BangEqual => Ok(Value::Boolean(!is_equal(&left_value, &right_value))),
+        _ => {
+            let description = format!("Operator '{}' is not valid for a binary expression", operator.token_type);
+            Err(RuntimeError::new(operator, description))
+        },
+    }
+}
+
+fn arithmetic(left: &Value, operator: Token, right: &Value) -> Result<Value, Box<Error>> {
+    match (left, right) {
+        (&Value::Number(l), &Value::Number(r)) => match operator.token_type {
+            TokenType::Minus => Ok(Value::Number(l - r)),
+            TokenType::Plus => Ok(Value::Number(l + r)),
+            TokenType::Slash if r == 0.0 => Err(RuntimeError::new(operator, String::from("Can't divide by zero"))),
+            TokenType::Slash => Ok(Value::Number(l / r)),
+            TokenType::Star => Ok(Value::Number(l * r)),
+            _ => {
+                let description = format!("Operator '{}' is not valid for arithmetic", operator.token_type);
+                Err(RuntimeError::new(operator, description))
+            },
+        },
+        (&Value::Str(ref l), _) => match operator.token_type {
+            TokenType::Plus => Ok(Value::Str(format!("{}{}", l, right.to_string()))),
+            _ => {
+                let description = format!("Operator '{}' is not valid for string concatenation", operator.token_type);
+                Err(RuntimeError::new(operator, description))
+            },
+        },
+        (_, &Value::Str(ref r)) => match operator.token_type {
+            TokenType::Plus => Ok(Value::Str(format!("{}{}", left.to_string(), r))),
+            _ => {
+                let description = format!("Operator '{}' is not valid for string concatenation", operator.token_type);
+                Err(RuntimeError::new(operator, description))
+            },
+        },
+        _ => Err(RuntimeError::new(operator, String::from("Cannot perform arithmetic on non-numeric values"))),
+    }
+}
+
+fn compare(left: &Value, operator: Token, right: &Value) -> Result<Value, Box<Error>> {
+    match (left, right) {
+        (&Value::Number(l), &Value::Number(r)) => match operator.token_type {
+            TokenType::Less => Ok(Value::Boolean(l < r)),
+            TokenType::LessEqual => Ok(Value::Boolean(l <= r)),
+            TokenType::Greater => Ok(Value::Boolean(l > r)),
+            TokenType::GreaterEqual => Ok(Value::Boolean(l >= r)),
+            _ => {
+                let description = format!("Operator '{}' is not valid for comparison", operator.token_type);
+                Err(RuntimeError::new(operator, description))
+            },
+        },
+        _ => Err(RuntimeError::new(operator, String::from("Cannot perform comparison on non-numeric values"))),
+    }
+}
+
+fn evaluate_grouping(state: &mut State, expression: Expr) -> Result<Value, Box<Error>> {
+    evaluate_expression(state, expression)
+}
+
+fn evaluate_literal(_state: &mut State, value: Value) -> Result<Value, Box<Error>> {
+    Ok(value)
+}
+
+fn evaluate_unary(state: &mut State, operator: Token, right: Expr) -> Result<Value, Box<Error>> {
+    let right_value = evaluate_expression(state, right)?;
+    match operator.token_type {
+        TokenType::Minus => match right_value {
+            Value::Number(n) => Ok(Value::Number(-n)),
+            _ => Err(RuntimeError::new(operator, format!("Operator '-' cannot be applied to non-number value {}", right_value))),
+        },
+        TokenType::Bang => Ok(Value::Boolean(!is_truthy(&right_value))),
+        _ => {
+            let description = format!("Operator '{}' is not valid in a unary expression", operator.token_type);
+            Err(RuntimeError::new(operator, description))
+        },
+    }
+}
+
+fn is_truthy(value: &Value) -> bool {
+    match value {
+        &Value::Nil => false,
+        &Value::Boolean(b) => b,
+        _ => true,
+    }
+}
+
+fn is_equal(left: &Value, right: &Value) -> bool {
+    match (left, right) {
+        (&Value::Nil, &Value::Nil) => true,
+        (&Value::Nil, _) => false,
+        (_, _) => left == right,
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,24 @@ use std::error::Error;
 
 pub mod callable;
 pub mod expression;
+pub mod interpreter;
 pub mod function;
 pub mod parser;
 pub mod scanner;
 pub mod token;
 pub mod value;
 
-use expression::Expr;
+use interpreter::interpret;
 use parser::parse;
 use scanner::scan;
+use value::Value;
 
-pub fn run(source: &String) -> Result<Expr, Box<Error>> {
+pub fn run(source: &String) -> Result<Value, Box<Error>> {
     scan(source).and_then(|tokens| {
         println!("tokens: {:?}", tokens);
         parse(tokens)
+    }).and_then(|expr| {
+        println!("expr: {}", expr);
+        interpret(expr)
     })
 }
